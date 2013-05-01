3cd9956095293451609b29ced40561a374c8708e
cfg <cfg@cfg-ThinkPad-X220.(none)>
2013-05-01 20:40:43 +0800

    jquery test

--- main.py
+++ main.py
@@ -30,29 +30,6 @@ def checktime(p):
 
 @app.route("/marknote/<orderby>/<int:page>")
 def main(orderby,page):
-    '''
-    global dec
-    if orderby == 'time':
-        if dec == 1:
-            order = desc(MarkNote.time)
-        else:
-            order = MarkNote.time
-    elif orderby == 'title':
-        dec = 0
-        order = MarkNote.title
-    elif orderby == 'tag':
-        dec = 0
-        data = qu.query(Tag,Tag.id)
-        return render_template('tags_page.html',data=data,\
-                                page=page-((page-1)%5),orderby='tag')
-    elif orderby == 'random':
-        #a = func.rand() for mysql
-        order = func.random()
-        dec = 0
-    else:
-        order = desc(MarkNote.time)
-        dec = 1
-    '''
     data = get_data(orderby,page)
     #data = qu.query(MarkNote,order,num=10,offset=(page-1)*10)
     print qu.count(MarkNote)
@@ -62,22 +39,12 @@ def main(orderby,page):
     return render_template('main.html',data=data,\
                             page=page-((page-1)%5),orderby=orderby,curp=page)
 
-@app.route("/marknote/tags/<tag_title>")
-def tag_page(tag_title):
-    rel = qu.query_bytitle(Tag,tag_title)
-    note = []
-    for a in rel.relates:
-        note += [qu.query_byid(a.marknoteid)]
-        print a.title,a.tagid,a.marknoteid
-    for a in note:
-        print a.title, a.tag
-    return render_template('tags.html',data=note)
-
-@app.route("/marknote/search",methods=["GET"])
-def search():
-    term = request.args.get('search','')
-    data = qu.search(term)
-    return render_template('search_page.html',data=data)
+@app.route("/marknote/get_load",methods=["GET"])
+def get_load():
+    orderby = request.args.get("orderby")
+    offset = request.args.get("offset")
+    data = get_data(orderby,int(offset))
+    return render_template('load.html',data=data)
 
 @app.route("/marknote/get_data",methods=["GET"])
 def get_data(orderby="time",page=1):
@@ -103,6 +70,23 @@ def get_data(orderby="time",page=1):
     print request.args.get
     return qu.query(MarkNote,order,num=10,offset=(page-1)*10)
 
+@app.route("/marknote/tags/<tag_title>")
+def tag_page(tag_title):
+    rel = qu.query_bytitle(Tag,tag_title)
+    note = []
+    for a in rel.relates:
+        note += [qu.query_byid(a.marknoteid)]
+        print a.title,a.tagid,a.marknoteid
+    for a in note:
+        print a.title, a.tag
+    return render_template('tags.html',data=note)
+
+@app.route("/marknote/search",methods=["GET"])
+def search():
+    term = request.args.get('search','')
+    data = qu.search(term)
+    return render_template('search_page.html',data=data)
+
 @app.route("/marknote/get_test",methods=["GET","POST"])
 def get_test():
     if request.method == "GET":
@@ -113,6 +97,6 @@ def get_test():
     return "get success"
 
 if __name__=='__main__':
-    #app.debug = True
-    #app.run(host='127.0.0.1')
-    app.run(host='192.168.1.2')
+    app.debug = True
+    app.run(host='127.0.0.1')
+    #app.run(host='192.168.1.2')
